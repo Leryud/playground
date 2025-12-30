9cd3a611396c976f3bedcad7792aa7ae212b8261
Léo Innocenzi <leo.innocenzi@gmail.com>
2025-12-30 21:09:09 +0100

    Register all experiments

---
+++ experiments/index.ts
@@ -0,0 +1,25 @@
+import { registerExperiment } from '@/lib/experiments/registry';
+
+import { Counter } from './counter/index';
+import { metadata as counterMetadata } from './counter/metadata';
+
+import { ColorPicker } from './color-picker/index';
+import { metadata as colorPickerMetadata } from './color-picker/metadata';
+
+import { TextTransformer } from './text-transformer/index';
+import { metadata as textTransformerMetadata } from './text-transformer/metadata';
+
+registerExperiment({
+  metadata: counterMetadata,
+  component: Counter,
+});
+
+registerExperiment({
+  metadata: colorPickerMetadata,
+  component: ColorPicker,
+});
+
+registerExperiment({
+  metadata: textTransformerMetadata,
+  component: TextTransformer,
+});
